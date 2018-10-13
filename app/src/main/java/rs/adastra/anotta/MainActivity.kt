5906339f733ac3cb2cc94f7d2616628da6a412c5
aleksandar-stefanovic <theonewithideas@gmail.com>
2018-10-13 14:28:56 +0200

    Added Architecture Components dependencies

--- app/src/main/java/rs/adastra/anotta/MainActivity.kt
+++ app/src/main/java/rs/adastra/anotta/MainActivity.kt
@@ -2,7 +2,7 @@ package rs.adastra.anotta
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
